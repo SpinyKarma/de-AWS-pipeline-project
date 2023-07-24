daba5eaa5518dee19d895768ed5e8fb797ec64aa
Jamie Pendergast <jamiependergast@gmail.com>
2023-07-24 15:28:02 +0100

    will create a connection to the db

--- src/ingestion.py
+++ src/ingestion.py
@@ -1,23 +1,26 @@
-import pg8000
-import boto3
+import converter
 import json
+import pg8000.native as pg8000
+from pprint import pprint
+# import boto3
+boto3 = None
 
 
-secretsmanager = boto3.client('secretsmanager')
-
-
+"""
 def get_credentials():
-    """
         Will return a dictionary containing
         our credentials (as defined in converter.py)
-    """
-
-    secret_name = 'ingestion_credentials'
+    secretsmanager = boto3.client('secretsmanager')
+    secret_name = 'Ingestion_credentials'
     credentials_response = secretsmanager.get_secret_value(
         SecretId=secret_name
     )
     credentials = json.loads(credentials_response['SecretString'])
     return credentials
+"""
+
+hostname = 'nc-data-eng-totesys-production'
+hostname += '.chpsczt8h1nu.eu-west-2.rds.amazonaws.com'
 
 
 def connect():
@@ -25,11 +28,27 @@ def connect():
         Will return a connection to the ingestion database
     """
 
-    credentials = get_credentials()
-    return pg8000.connect(
+#    credentials = get_credentials()
+    credentials = converter.convert_ingestion_credentials(
+        hostname,
+        port='5432',
+        db='totesys',
+        username='project_user_5',
+        password='FiA0ooxIw4ojnmcJmc8VwrWm'
+    )
+
+    credentials = json.loads(credentials)
+
+    return pg8000.Connection(
         user=credentials['username'],
         password=credentials['password'],
         host=credentials['hostname'],
         database=credentials['db'],
         port=credentials['port']
     )
+
+
+if __name__ == '__main__':
+    with connect() as db:
+        result = db.run('SELECT * FROM staff;')
+        pprint([column['name'] for column in db.columns])
