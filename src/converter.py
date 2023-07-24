f4de3c881e2ceb002dce85382b0ca91a6bbf8925
SpinyKarma <samuel.shaw52@gmail.com>
2023-07-24 14:21:07 +0100

    pep8 compliance

--- src/converter.py
+++ src/converter.py
@@ -1,65 +1,61 @@
 import json
 
 
-class NullCredentialsError (Exception):
+class NullCredentialsError(Exception):
     """
-        Raised when a required credential is None
+    Raised when a required credential is None
     """
+
     pass
 
 
 def convert_warehouse_credentials(port, schema, user, password):
     """
-        Returns a string: the JSON representation of our credentials
+    Returns a string: the JSON representation of our credentials
     """
 
     if port is None:
-        raise NullCredentialsError('port')
+        raise NullCredentialsError("port")
 
     if schema is None:
-        raise NullCredentialsError('schema')
+        raise NullCredentialsError("schema")
 
     if user is None:
-        raise NullCredentialsError('user')
+        raise NullCredentialsError("user")
 
     if password is None:
-        raise NullCredentialsError('password')
+        raise NullCredentialsError("password")
 
-    credentials = {
-        'port': port,
-        'schema': schema,
-        'user': user,
-        'password': password
-    }
+    credentials = {"port": port, "schema": schema, "user": user, "password": password}
 
     return json.dumps(credentials)
 
 
 def convert_ingestion_credentials(hostname, port, db, username, password):
     """
-        Returns a string: the JSON representation of our credentials 
+    Returns a string: the JSON representation of our credentials
     """
     if hostname is None:
-        raise NullCredentialsError('hostname')
+        raise NullCredentialsError("hostname")
 
     if port is None:
-        raise NullCredentialsError('port')
+        raise NullCredentialsError("port")
 
     if db is None:
-        raise NullCredentialsError('db')
+        raise NullCredentialsError("db")
 
     if username is None:
-        raise NullCredentialsError('username')
+        raise NullCredentialsError("username")
 
     if password is None:
-        raise NullCredentialsError('password')
+        raise NullCredentialsError("password")
 
     credentials = {
-        'hostname': hostname,
-        'port': port,
-        'db': db,
-        'username': username,
-        'password': password
+        "hostname": hostname,
+        "port": port,
+        "db": db,
+        "username": username,
+        "password": password,
     }
 
     return json.dumps(credentials)
--- test/test_converter.py
+++ test/test_converter.py
@@ -4,52 +4,61 @@ import src.converter as converter
 
 def test_convert_warehouse_credentials_raises_NullCredentialsError_on_null_credential():
     with pytest.raises(converter.NullCredentialsError):
-        converter.convert_warehouse_credentials(
-            None, 'schema', 'user', 'password')
+        converter.convert_warehouse_credentials(None, "schema", "user", "password")
 
     with pytest.raises(converter.NullCredentialsError):
-        converter.convert_warehouse_credentials(
-            'port', None, 'user', 'password')
+        converter.convert_warehouse_credentials("port", None, "user", "password")
 
     with pytest.raises(converter.NullCredentialsError):
-        converter.convert_warehouse_credentials(
-            'port', 'schema', None, 'password')
+        converter.convert_warehouse_credentials("port", "schema", None, "password")
 
     with pytest.raises(converter.NullCredentialsError):
-        converter.convert_warehouse_credentials(
-            'port', 'schema', 'user', None)
+        converter.convert_warehouse_credentials("port", "schema", "user", None)
 
 
 def test_convert_warehouse_credentials_returns_valid_json():
     example = '{"port": "1234", "schema": "exampleschema", "user": "exampleuser", "password": "examplepassword"}'
 
-    assert converter.convert_warehouse_credentials(
-        '1234', 'exampleschema', 'exampleuser', 'examplepassword') == example
+    assert (
+        converter.convert_warehouse_credentials(
+            "1234", "exampleschema", "exampleuser", "examplepassword"
+        )
+        == example
+    )
 
 
 def test_convert_ingestion_credentials_raises_NullCredentialsError_on_null_credential():
     with pytest.raises(converter.NullCredentialsError):
         converter.convert_ingestion_credentials(
-            None, 'port', 'db', 'username', 'password')
+            None, "port", "db", "username", "password"
+        )
 
     with pytest.raises(converter.NullCredentialsError):
         converter.convert_ingestion_credentials(
-            'hostname', None, 'db', 'username', 'password')
+            "hostname", None, "db", "username", "password"
+        )
 
     with pytest.raises(converter.NullCredentialsError):
         converter.convert_ingestion_credentials(
-            'hostname', 'port', None, 'username', 'password')
+            "hostname", "port", None, "username", "password"
+        )
 
     with pytest.raises(converter.NullCredentialsError):
         converter.convert_ingestion_credentials(
-            'hostname', 'port', 'db', None, 'password')
+            "hostname", "port", "db", None, "password"
+        )
 
     with pytest.raises(converter.NullCredentialsError):
         converter.convert_ingestion_credentials(
-            'hostname', 'port', 'db', 'username', None)
+            "hostname", "port", "db", "username", None
+        )
 
 
 def test_convert_ingestion_wraehouse_returns_valid_json():
     example = '{"hostname": "examplehostname", "port": "1234", "db": "exampledb", "username": "exampleusername", "password": "examplepassword"}'
-    assert converter.convert_ingestion_credentials(
-        'examplehostname', '1234', 'exampledb', 'exampleusername', 'examplepassword') == example
+    assert (
+        converter.convert_ingestion_credentials(
+            "examplehostname", "1234", "exampledb", "exampleusername", "examplepassword"
+        )
+        == example
+    )
