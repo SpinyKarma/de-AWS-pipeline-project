796f4943e423e8ddbd0fcbb3ca5c8891ad191873
Andrew <Andrewjackkelley638@gmail.com>
2023-07-25 10:27:47 +0100

    ordering change and spelling errors

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,10 +3,11 @@ resource "aws_s3_bucket" "raw_csv_data_bucket" {
     bucket_prefix = "terrific-totes-ingestion-bucket"
 }
 
+resource "aws_s3_bucket" "code_bucket" {
+    bucket_prefix = "lambda-code-bucket"
+}
+
 resource "aws_s3_bucket" "processed-parquet-data" {
     bucket_prefix = "terrific-totes-processed-bucket"
 }
 
-resource "aws_s3_bucket" "code_bucket" {
-    bucket_prefix = "lambda-code-bucket"
-}
