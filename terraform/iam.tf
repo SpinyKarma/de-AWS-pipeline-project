9dea6f622ef125a6f7905dd563240433c8ecf26a
SpinyKarma <samuel.shaw52@gmail.com>
2023-07-25 09:59:16 +0100

    feat(iam.tf): appended the secret read permission to the role the lambda will assume

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,15 +1,25 @@
-##########################
-####  POLICY CREATION ####
-##########################
+###########################
+####  POLICY CREATION  ####
+###########################
 
-data "aws_iam_policy_document" "s3_read_policy_document" {
+data "aws_iam_policy_document" "s3_write_policy_document" {
   statement {
 
-    actions = ["s3:GetObject"]
+    actions = ["s3:PutObject"]
 
     resources = [
-      "${aws_s3_bucket.code_bucket.arn}/*",
-      "${aws_s3_bucket.data_bucket.arn}/*",
+      #"${aws_s3_bucket.@@@.arn}/*", #replace @@@ with ingestion bucket label then uncomment
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "secret_read_policy_document" {
+  statement {
+
+    actions = ["secretsmanager:GetSecretValue"]
+
+    resources = [
+      "arn:aws:secretsmanager:eu-west-2:967289384825:secret:*"
     ]
   }
 }
@@ -20,7 +30,7 @@ data "aws_iam_policy_document" "cw_write_policy_document" {
     actions = ["logs:CreateLogGroup"]
 
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:*"
+      "arn:aws:logs:eu-west-2:967289384825:*"
     ]
   }
 
@@ -29,25 +39,29 @@ data "aws_iam_policy_document" "cw_write_policy_document" {
     actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
 
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.lambda_name}:*"
+      "arn:aws:logs:eu-west-2:967289384825:log-group:/aws/lambda/${var.lambda_name}:*"
     ]
   }
 }
 
-resource "aws_iam_policy" "s3_read_policy" {
-  name_prefix = "s3-read-policy-${var.lambda_name}" #change to actual lambda name when lambda is deployed
-  policy      = data.aws_iam_policy_document.s3_read_policy_document.json
+resource "aws_iam_policy" "s3_write_policy" {
+  name_prefix = "s3-write-policy-${var.lambda_name}" #change to actual lambda name when lambda is deployed
+  policy      = data.aws_iam_policy_document.s3_write_policy_document.json
 }
 
+resource "aws_iam_policy" "secret_read_policy" {
+  name_prefix = "secret-read-policy-${var.lambda_name}" #change to actual lambda name when lambda is deployed
+  policy      = data.aws_iam_policy_document.secret_read_policy_document.json
+}
 
 resource "aws_iam_policy" "cw_write_policy" {
   name_prefix = "cw-write-policy-${var.lambda_name}" #change to actual lambda name when lambda is deployed
   policy      = data.aws_iam_policy_document.cw_write_policy_document.json
 }
 
-###########################
-####  INGESTION LAMBDA ####
-###########################
+############################
+####  INGESTION LAMBDA  ####
+############################
 
 resource "aws_iam_role" "ingestion_lambda_role" {
   name_prefix        = "role-${var.lambda_name}" #change to actual lambda name when lambda is deployed
@@ -71,9 +85,14 @@ resource "aws_iam_role" "ingestion_lambda_role" {
     EOF
 }
 
-resource "aws_iam_role_policy_attachment" "ingestion_s3_read_policy_attachment" {
+resource "aws_iam_role_policy_attachment" "ingestion_s3_write_policy_attachment" {
   role       = aws_iam_role.ingestion_lambda_role.name
-  policy_arn = aws_iam_policy.s3_read_policy.arn
+  policy_arn = aws_iam_policy.s3_write_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ingestion_secret_read_policy_attachment" {
+  role       = aws_iam_role.ingestion_lambda_role.name
+  policy_arn = aws_iam_policy.secret_read_policy.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ingestion_cw_write_policy_attachment" {
