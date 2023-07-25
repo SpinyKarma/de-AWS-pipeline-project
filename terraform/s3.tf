0d89e3fb6db4a426c7279ea64a8ca2239d47f388
Andrew <Andrewjackkelley638@gmail.com>
2023-07-25 10:13:11 +0100

    spelling errors and added code_bucket for all lambdas to be hosted in

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -3,7 +3,10 @@ resource "aws_s3_bucket" "raw_csv_data_bucket" {
     bucket_prefix = "terrific-totes-ingestion-bucket"
 }
 
-resource "aws_s3_bucket" "processed-paraquet-data" {
-    bucket_prefix = "terrifc-totes-processed-bucket"
+resource "aws_s3_bucket" "processed-parquet-data" {
+    bucket_prefix = "terrific-totes-processed-bucket"
 }
 
+resource "aws_s3_bucket" "code_bucket" {
+    bucket_prefix = "lambda-code-bucket"
+}
