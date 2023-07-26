b3cfba6d545b005f3adb2477c5c7aee5e6712e75
SpinyKarma <samuel.shaw52@gmail.com>
2023-07-26 16:43:09 +0100

    reworked to use timestamps from table data

--- test/test_timestamp.py
+++ test/test_timestamp.py
@@ -1,17 +1,21 @@
 from datetime import datetime as dt
 import boto3
 from moto import mock_s3
-from lambda_ingestion.ingestion import (
-    get_last_ingestion_timestamps,
-    set_last_ingestion_timestamps,
-    # ingest
+from src.lambda_ingestion.ingestion import (
+    get_ingestion_timestamps,
+    # extract_table_to_csv
 )
+# from unittest.mock import Mock
 # function returning a sample csv string for testing
+current_timestamp = dt.now().isoformat()
 
 
 def generate_csv_string():
-    value = "transaction_id,transaction_type\n"
-    value += "434,SALE,435,PURCHASE,436,SALE,437,PURCHASE\n"
+    value = "transaction_id,transaction_type,last_updated\n"
+    value += f"434,SALE,{current_timestamp}\n"
+    value += f"435,PURCHASE,{current_timestamp}\n"
+    value += f"436,SALE,{current_timestamp}\n"
+    value += f"437,PURCHASE,{current_timestamp}\n"
     return value
 
 
@@ -25,37 +29,32 @@ def test_timestamp_functionality():
     # grab csv string
     csv_data = generate_csv_string()
     # get current timestamp
-    current_timestamp = dt.now().isoformat()
+
     s3_client.put_object(
         Body=csv_data,
         Bucket=bucket_name,
         Key='test_table.csv',
         ContentType='application/text',
-        Metadata={'LastIngestionTimestamp': current_timestamp}
     )
-    # retrieve last ingestion timestamps again
-    updated_last_ingestion_timestamps = get_last_ingestion_timestamps(
-        s3_client=s3_client, Bucket=bucket_name)
-    # check if the timestamp
-    # for the test table was updated with new current timestamp
-    assert 'test_table' in updated_last_ingestion_timestamps
-    assert isinstance(updated_last_ingestion_timestamps['test_table'], dt)
+    list = get_ingestion_timestamps(s3_client, bucket_name, ['test_table'])
+    assert list['test_table'].isoformat() == current_timestamp
 
-
-@mock_s3
-def test_timestamp_set():
-    bucket_name = "test_ingestion_bucket"
-    s3_client = boto3.client('s3', region_name='us-east-1')
-    s3_client.create_bucket(Bucket=bucket_name)
-    s3_client.put_object(
-        Body='csv_data',
-        Bucket=bucket_name,
-        Key='test_table.csv',
-        ContentType='application/text',
-    )
-    set_last_ingestion_timestamps(get_last_ingestion_timestamps(
-        s3_client=s3_client, Bucket=bucket_name), s3_client=s3_client, Bucket=bucket_name)
-    response = s3_client.get_object(Bucket=bucket_name, Key='test_table.csv')
-    metadata = response.get('Metadata')
-    # assert metadata == False
-    assert metadata['lastingestiontimestamp'] == '2023-07-26T12:03:34.650508'
+# Mocks don't support context manager so don't know how to test
+# def test_extract_table_to_csv_concats_query_result_to_csv():
+#     mock_connection = Mock()
+#     mock_db = Mock()
+#     mock_connection.return_value = mock_db
+#     mock_db.run.return_value = [
+#         ["transaction_id", "transaction_type", "last_updated"],
+#         ["434", "SALE", current_timestamp],
+#         ["435", "PURCHASE", current_timestamp],
+#         ["436", "SALE", current_timestamp],
+#         ["437", "PURCHASE", current_timestamp],
+#     ]
+#     res = extract_table_to_csv("test_table", dt(1970, 1, 1), mock_connection)
+#     expected = "transaction_id, transaction_type, last_updated\r\n"
+#     expected += f"434, SALE, {current_timestamp}\r\n"
+#     expected += f"435, PURCHASE, {current_timestamp}\r\n"
+#     expected += f"436, SALE, {current_timestamp}\r\n"
+#     expected += f"437, PURCHASE, {current_timestamp}\r\n"
+#     assert res == expected
