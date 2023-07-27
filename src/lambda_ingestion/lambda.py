374d1091d824754bbfd30122b640f738eafc77d0
SpinyKarma <samuel.shaw52@gmail.com>
2023-07-27 14:22:53 +0100

    use utils to implement lambda functionality

--- src/lambda_ingestion/lambda.py
+++ src/lambda_ingestion/lambda.py
@@ -2,14 +2,26 @@ import boto3
 import ingestion as i
 import logging
 
-def ingestion_lambda_handler(event,context):
+
+def ingestion_lambda_handler(event, context):
+    '''
+    '''
     try:
-        s3=boto3.client('s3')
-        i.ingest(s3)
+        table_names = [
+            'staff',
+            'counterparty',
+            'sales_order',
+            'address',
+            'payment',
+            'purchase_order',
+            'payment_type',
+            'transaction',
+        ]
+        buckey_name = i.get_ingestion_bucket_name()
+        i.postgres_to_csv(buckey_name, table_names)
     except i.TableIngestionError as error:
         logging.error(f'Table Ingestion Error: {error}')
     except i.InvalidCredentialsError as error:
         logging.error(f'Invalid Credentials Error: {error}')
     except Exception as e:
         logging.error(f'An unexpected error occured: {e}')
-
