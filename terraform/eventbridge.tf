0ce410e3c86b05fb78b1a58c53e3252a243c71e0
SpinyKarma <samuel.shaw52@gmail.com>
2023-07-27 16:25:13 +0100

    ingestion lambda works?

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,19 +1,19 @@
 resource "aws_cloudwatch_event_rule" "ingestion_lambda_rule" {
-    name="ingestion_lambda_rule"
-    schedule_expression = "rate(3 minutes)"
+  name                = "ingestion_lambda_rule"
+  schedule_expression = "rate(3 minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "ingestion_lambda_target" {
-  rule=aws_cloudwatch_event_rule.ingestion_lambda_rule.name
+  rule      = aws_cloudwatch_event_rule.ingestion_lambda_rule.name
   target_id = "SendToLambda"
-  arn= aws_lambda_function.ingestion_lambda.arn
+  arn       = aws_lambda_function.ingestion_lambda.arn
 }
 
 resource "aws_lambda_permission" "ingestion_lambda_event" {
-  statement_id = "AllowExecutionFromEventBridge"
-  action = "lambda:InvokeFunction"
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
   function_name = "ingestion_lambda_handler"
-  principal = "events.amazonaws.com"
-  source_arn = aws_cloudwatch_event_rule.ingestion_lambda_rule.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ingestion_lambda_rule.arn
 }
 
