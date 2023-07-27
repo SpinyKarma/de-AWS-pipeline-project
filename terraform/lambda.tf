10e69961fba56855c1c39b9c17e11ab43c9c7e92
SpinyKarma <samuel.shaw52@gmail.com>
2023-07-27 15:08:23 +0100

    migrate ingestion lambda dependencies to same file

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ locals {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/../src/lambda_ingestion"
+  source_file = "${path.module}/../src/lambda_ingestion/ingestion_lambda.py"
   output_path = "${path.module}/../lambda_ingestion.zip"
 }
 
@@ -20,7 +20,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   filename      = "../lambda_ingestion.zip"
   function_name = "ingestion_lambda_handler"
   role          = aws_iam_role.ingestion_lambda_role.arn
-  handler       = "lambda.ingestion_lambda_handler"
+  handler       = "ingestion_lambda.ingestion_lambda_handler"
   #handler may need to be "lambda_ingestion.lambda.ingestion_lambda_handler"
   runtime = "python3.10"
 }
