654eeb2e3a7ceae2b13c1b1e601ede0d0edd98f2
SpinyKarma <samuel.shaw52@gmail.com>
2023-07-27 14:36:58 +0100

    change lambda source to include dependencies

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,24 +3,24 @@ locals {
 }
 
 data "archive_file" "lambda_zip" {
-    type = "zip"
-    source_file = "${path.module}/../src/lambda_ingestion/lambda.py"
-    output_path = "${path.module}/../lambda_ingestion.zip"
+  type        = "zip"
+  source_dir  = "${path.module}/../src/lambda_ingestion"
+  output_path = "${path.module}/../lambda_ingestion.zip"
 }
 
 resource "aws_s3_object" "lambda_code" {
-    bucket = aws_s3_bucket.code_bucket.bucket
-    key = "lambda_ingestion.zip"
-    acl = "private"
+  bucket = aws_s3_bucket.code_bucket.bucket
+  key    = "lambda_ingestion.zip"
+  acl    = "private"
 
-    source = data.archive_file.lambda_zip.output_path
+  source = data.archive_file.lambda_zip.output_path
 }
 
-resource "aws_lambda_function" "ingestion_lambda" {  
-  filename = "../lambda_ingestion.zip"
+resource "aws_lambda_function" "ingestion_lambda" {
+  filename      = "../lambda_ingestion.zip"
   function_name = "ingestion_lambda_handler"
-  role = aws_iam_role.ingestion_lambda_role.arn
-  
-  handler = "lambda.ingestion_lambda_handler"
+  role          = aws_iam_role.ingestion_lambda_role.arn
+  handler       = "lambda.ingestion_lambda_handler"
+  #handler may need to be "lambda_ingestion.lambda.ingestion_lambda_handler"
   runtime = "python3.10"
 }
