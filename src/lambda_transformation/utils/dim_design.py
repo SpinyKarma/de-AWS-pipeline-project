a221916f8b0718025c1544c6d963e291b2949d7c
Robb Fox <75976583+robbfox@users.noreply.github.com>
2023-07-28 15:41:52 +0100

    Update dim_design.py

--- src/lambda_transformation/utils/dim_design.py
+++ src/lambda_transformation/utils/dim_design.py
@@ -5,8 +5,7 @@ s3 = boto3.client('s3')
 def create_design_parquet(key, ingestion_bucket, parquet_bucket):
     response = s3.get_object(Bucket=ingestion_bucket, Key=key)
     df = pd.read_csv(response['Body'])
-    df = pd.drop(columns=['last_updated'])
-    df = pd.drop(columns=['created_at'])
+    df = pd.drop(columns=['last_updated', 'created_at'])
     new_design = df.to_parquet()
     s3.put_object(Bucket=parquet_bucket, Key='dim_design.parquet', Body=new_design)
 
