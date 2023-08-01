cd1736884ae25913abfc4d84b9bcf6ebe58b518c
SpinyKarma <samuel.shaw52@gmail.com>
2023-08-01 14:17:56 +0100

    commiting because branch switching

--- src/lambda_ingestion/connection.py
+++ src/lambda_ingestion/connection.py
@@ -1,5 +1,9 @@
 from ingestion_lambda import connect
+from pprint import pprint
 
 with connect() as db:
-    res = db.run("SELECT * FROM project_user_5;")
-    print(res)
+    res = db.run(
+        "SELECT * FROM information_schema.tables WHERE table_schema is ;")
+    cols = [item['name'] for item in db.columns]
+    pprint(cols)
+    print(len(res))
