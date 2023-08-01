5bd2ff034c274bd8e49c27f444dada47eee45c25
SpinyKarma <samuel.shaw52@gmail.com>
2023-08-01 14:44:58 +0100

    connection function in ingestion lambda now works for warehouse as well

--- src/lambda_ingestion/ingestion_lambda.py
+++ src/lambda_ingestion/ingestion_lambda.py
@@ -116,7 +116,7 @@ def connect(db="ingestion"):
             user=credentials['username'],
             password=credentials['password'],
             host=credentials['hostname'],
-            database=credentials['postgres'],
+            database='postgres',
             port=credentials['port']
         )
     else:
