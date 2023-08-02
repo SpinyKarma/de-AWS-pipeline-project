f70935981a5ab1e4eec181b899b6fe4d8a45f6bc
Jamie Pendergast <jamiependergast@gmail.com>
2023-08-02 11:14:09 +0100

    will now check for expected columns

--- test/lambda_transformation/utils/test_dim_staff.py
+++ test/lambda_transformation/utils/test_dim_staff.py
@@ -1,5 +1,6 @@
 import src.lambda_transformation.utils.dim_staff as util
 import src.table_utils.get_tables as tables
+from pandas import DataFrame
 
 
 def test_timestamp_is_preserved():
@@ -27,4 +28,32 @@ def test_timestamp_is_preserved():
     assert result['Key'].startswith(recent_timestamp.isoformat())
 
 
-test_timestamp_is_preserved()
+def test_body_is_a_pd_dataframe():
+    result: DataFrame = util.create_dim_staff_csv(
+        tables.read_table('staff.csv'),
+        tables.read_table('department.csv')
+    )
+    assert type(result['Body']) is DataFrame
+
+
+def test_body_is_as_expected():
+    staff = tables.read_table('staff.csv')
+    depts = tables.read_table('department.csv')
+    result = util.create_dim_staff_csv(staff, depts)
+    body = result['Body']
+
+    '''
+        As according to the schema
+    '''
+
+    expected_columns = [
+        'staff_id',
+        'first_name',
+        'last_name',
+        'department_name',
+        'location',
+        'email_address'
+    ]
+
+    for column in expected_columns:
+        assert column in body.columns
