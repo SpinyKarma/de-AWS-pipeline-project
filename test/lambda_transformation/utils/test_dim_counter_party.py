ed86b56d3beeda435b6b5ae54dcea30be73aa547
Ali Mohamed <alifanan@yahoo.com>
2023-08-02 12:28:37 +0100

    testing missing the example dict for address and counterparty

--- test/lambda_transformation/utils/test_dim_counter_party.py
+++ test/lambda_transformation/utils/test_dim_counter_party.py
@@ -1,6 +1,7 @@
 import pytest
 import src.lambda_transformation.utils.dim_counter_party as util
- import src.table_utils.get_tables as tables
+import src.table_utils.get_tables as tables
+import pandas as pd
 
 
 def test_that_the_function_return_a_dict_with_desired_keys():
@@ -10,19 +11,43 @@ def test_that_the_function_return_a_dict_with_desired_keys():
     assert isinstance(result, dict)
 
 def test_that_body_for_both_address_and_counterparty_are_DataFrame_Dict(): 
-    pass
+    address_dict={
+       'Key':'address.csv',
+       'Body':pd.DataFrame({ 
+           
+       })
+       
+    }
+    counterparty_dict={
+        'Key':'address.csv',
+        'Body':pd.DataFrame({ 
+
+        })
+       }
+    result = util.counter_party_address_to_dim_counterparty(counterparty_dict,address_dict)
+    assert isinstance(result['Body'],pd.DataFrame)
+    
 
 def test_dim_counterparty_dict_contains_all_the_expected_keys():
-    pass
+    address_dict={
+       'Key':'address.csv',
+       'Body':pd.DataFrame({ 
+           
+       })
+       
+    }
+    counterparty_dict={
+        'Key':'address.csv',
+        'Body':pd.DataFrame({ 
 
-test that timestamp is carried over to output
-test that output Body key a pd test_that_body_for_both_address_and_counterparty_are_DataFrame_Dict
- test that output body has had the expected transformation applied 
+        })
+       }
+    with pytest.raises(KeyError):
+        util.counter_party_address_to_dim_counterparty(counterparty_dict,address_dict)
 
 
-
-# if __name__ == '__main__':
-#     address = tables.read_table('address.csv')
-#     counterparty = tables.read_table('counterparty.csv')
-#     result = util.counter_party_address_to_dim_counterparty(counterparty,address) 
-#     print(result)
+if __name__ == '__main__':
+    address = tables.read_table('address.csv')
+    counterparty = tables.read_table('counterparty.csv')
+    result = util.counter_party_address_to_dim_counterparty(counterparty,address) 
+    print(result)
