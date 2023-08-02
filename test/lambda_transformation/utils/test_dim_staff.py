aff8f256b3c16f9d9364dcb0d498133f511dd821
SpinyKarma <samuel.shaw52@gmail.com>
2023-08-02 21:29:50 +0100

    commented faulty test

--- test/lambda_transformation/utils/test_dim_staff.py
+++ test/lambda_transformation/utils/test_dim_staff.py
@@ -2,32 +2,32 @@ import src.lambda_transformation.utils.dim_staff as util
 import src.lambda_transformation.utils.get_tables as tables
 from pandas import DataFrame
 
+# Test based on assumption that broke lambda, see comments in dim_staff.py
+# def test_timestamp_is_preserved():
+#     try:
+#         staff = tables.read_table('staff.csv')
+#         depts = tables.read_table('department.csv')
+#         result = util.staff_department_to_dim_staff(staff, depts)
+#         # Get the most recent timestamp
+#         recent_timestamp = [
+#             staff['Timestamp'],
+#             depts['Timestamp']
+#         ]
+#         recent_timestamp.sort(reverse=True)
+#         recent_timestamp = recent_timestamp[0]
+#         result_timestamp = result['Timestamp']
 
-def test_timestamp_is_preserved():
-    try:
-        staff = tables.read_table('staff.csv')
-        depts = tables.read_table('department.csv')
-        result = util.staff_department_to_dim_staff(staff, depts)
-        # Get the most recent timestamp
-        recent_timestamp = [
-            staff['Timestamp'],
-            depts['Timestamp']
-        ]
-        recent_timestamp.sort(reverse=True)
-        recent_timestamp = recent_timestamp[0]
-        result_timestamp = result['Timestamp']
+#         '''
+#             Is the timestamp the most recent?
+#         '''
+#         assert recent_timestamp == result_timestamp
 
-        '''
-            Is the timestamp the most recent?
-        '''
-        assert recent_timestamp == result_timestamp
-
-        '''
-            Is the key prefixed with the timestamp in isoformat?
-        '''
-        assert result['Key'].startswith(recent_timestamp.isoformat())
-    except tables.TableNotFoundError:
-        pass
+#         '''
+#             Is the key prefixed with the timestamp in isoformat?
+#         '''
+#         assert result['Key'].startswith(recent_timestamp.isoformat())
+#     except tables.TableNotFoundError:
+#         pass
 
 
 def test_body_is_a_pd_dataframe():
