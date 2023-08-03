bbb30fc883faf2630d1e16c5553cc79b6fff64b9
Jamie Pendergast <jamiependergast@gmail.com>
2023-08-03 09:08:32 +0100

    will fix test

--- test/lambda_transformation/utils/test_dim_staff.py
+++ test/lambda_transformation/utils/test_dim_staff.py
@@ -2,32 +2,27 @@ import src.lambda_transformation.utils.dim_staff as util
 import src.lambda_transformation.utils.get_tables as tables
 from pandas import DataFrame
 
-# Test based on assumption that broke lambda, see comments in dim_staff.py
-# def test_timestamp_is_preserved():
-#     try:
-#         staff = tables.read_table('staff.csv')
-#         depts = tables.read_table('department.csv')
-#         result = util.staff_department_to_dim_staff(staff, depts)
-#         # Get the most recent timestamp
-#         recent_timestamp = [
-#             staff['Timestamp'],
-#             depts['Timestamp']
-#         ]
-#         recent_timestamp.sort(reverse=True)
-#         recent_timestamp = recent_timestamp[0]
-#         result_timestamp = result['Timestamp']
 
-#         '''
-#             Is the timestamp the most recent?
-#         '''
-#         assert recent_timestamp == result_timestamp
+def test_timestamp_is_preserved():
+    try:
+        staff = tables.read_table('staff.csv')
+        depts = tables.read_table('department.csv')
+        result = util.staff_department_to_dim_staff(staff, depts)
+        # Get the most recent timestamp
+        recent_timestamp = [
+            staff['Timestamp'],
+            depts['Timestamp']
+        ]
+        result_timestamp = result['Timestamp']
 
-#         '''
-#             Is the key prefixed with the timestamp in isoformat?
-#         '''
-#         assert result['Key'].startswith(recent_timestamp.isoformat())
-#     except tables.TableNotFoundError:
-#         pass
+        # Are the timestamps the same?
+        assert recent_timestamp[0] == recent_timestamp[1]
+
+        # Is the timestamp of the result correct?
+        assert result_timestamp == recent_timestamp[0]
+
+    except tables.TableNotFoundError:
+        pass
 
 
 def test_body_is_a_pd_dataframe():
